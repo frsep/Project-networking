34b3746579cd78863b46f9384128207192b4b5c8
Sepehr <amid.sepehr@gmail.com>
2024-05-04 20:55:42 +0800

    c

--- server.c
+++ server.c
@@ -2,19 +2,29 @@
 #include <string.h>
 #include <stdlib.h> 
 #include <stdio.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
 
 struct client_server{
-    char *browser_port;
+    int browser_port;
     char name[256];
 };
 
 void server_listen(struct client_server *my_server){
     char client_req[1000];
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    int bind_sock = bind(sock, my_server->browser_port, sizeof(my_server->browser_port));
+    struct sockaddr_in _addr;
+    _addr.sin_family = AF_INET;
+    _addr.sin_addr.s_addr = INADDR_ANY;
+    _addr.sin_port = htons(my_server->browser_port);
+    
+
+    int bind_sock = bind(sock,(struct sockaddr*) &_addr, sizeof(struct sockaddr_in));
     int listen_sock = listen(sock, 1);
+    socklen_t addrlen = sizeof(_addr);
     while(1){
-        int accept_sock = accept(sock, my_server->browser_port, sizeof(my_server->browser_port));
+        int accept_sock = accept(sock, (struct sockaddr*) &_addr, &addrlen);
         read(accept_sock, client_req, sizeof(client_req));
         printf("%s", client_req);
         char respnce[] = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>";
@@ -25,8 +35,7 @@ void server_listen(struct client_server *my_server){
 
 int main(int argc, char const *argv[]){
     struct client_server my_server;
-
-    my_server.browser_port = argv[2];
+    my_server.browser_port = atoi(argv[2]);
     strcpy(my_server.name, argv[1]);
     server_listen(&my_server);
     return 0;
