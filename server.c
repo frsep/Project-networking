98efc184e5017159c93104b8f881cc2849b35ec6
Sepehr <amid.sepehr@gmail.com>
2024-05-04 20:01:03 +0800

    c

--- server.c
+++ server.c
@@ -1,16 +1,20 @@
 #include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <stdio.h>
 
 struct client_server{
-    int browser_port;
-    char *name;
+    char *browser_port;
+    char name[256];
 };
-void server_listen(struct client_server *server){
+
+void server_listen(struct client_server *my_server){
     char client_req[1000];
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    int bind_sock = bind(sock, server->browser_port, sizeof(server->browser_port));
+    int bind_sock = bind(sock, my_server->browser_port, sizeof(my_server->browser_port));
     int listen_sock = listen(sock, 1);
     while(1){
-        int accept_sock = accept(sock, server->browser_port, sizeof(server->browser_port));
+        int accept_sock = accept(sock, my_server->browser_port, sizeof(my_server->browser_port));
         read(accept_sock, client_req, sizeof(client_req));
         printf("%s", client_req);
         char respnce[] = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>";
@@ -21,8 +25,9 @@ void server_listen(struct client_server *server){
 
 int main(int argc, char const *argv[]){
     struct client_server my_server;
-    my_server.browser_port = atoi(argv[2]);
-    my_server.name = argv[1];
+
+    my_server.browser_port = argv[2];
+    strcpy(my_server.name, argv[1]);
     server_listen(&my_server);
     return 0;
 }
