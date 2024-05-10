c712fd141687091e769f1d9593649eeeff78a468
Sepehr <amid.sepehr@gmail.com>
2024-05-10 20:40:00 +0800

    c

--- recieve.c
+++ recieve.c
@@ -12,22 +12,38 @@
 struct client_server
 {// SEPEHR - could you please provide comments for what this struct is/does
     int browser_port;
+    int query_port;
     char name[MAX_WORDSIZE]; //SEPEHR- I've replaced with a pre-defined CONSTANT as best practise - delete this comment once read
 };
 
 
 void server_listen(struct client_server *my_server){
+
+    //  Create a socket and bind it to the browser port for TCP connection
     char client_req[1000];
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in _addr;
     _addr.sin_family = AF_INET;
     _addr.sin_addr.s_addr = INADDR_ANY;
     _addr.sin_port = htons(my_server->browser_port);
-    
-
     int bind_sock = bind(sock,(struct sockaddr*) &_addr, sizeof(struct sockaddr_in));
     int listen_sock = listen(sock, 1);
     socklen_t addrlen = sizeof(_addr);
+
+    // creates socket and binds it with the query port for UDP connection
+    int sock2 = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in udp_addr;
+    udp_addr.sin_family = AF_INET;
+    udp_addr.sin_addr.s_addr = INADDR_ANY;
+    udp_addr.sin_port = htons(my_server->query_port);
+    int bind_udpsock = bind(sock2,(struct sockaddr*) &udp_addr, sizeof(struct sockaddr_in));
+    int listen_udpsock = listen(sock2, 1);
+    socklen_t addrlen2 = sizeof(udp_addr);
+
+
+
+
+
     while(1){
         int accept_sock = accept(sock, (struct sockaddr*) &_addr, &addrlen);
         read(accept_sock, client_req, sizeof(client_req));
@@ -52,6 +68,7 @@ void server_listen(struct client_server *my_server){
 }
 int main(int argc, char const *argv[]){
     struct client_server my_server;
+    my_server.query_port = atoi(argv[3]);
     my_server.browser_port = atoi(argv[2]);
     strcpy(my_server.name, argv[1]);
     server_listen(&my_server);
--- server.c
+++ server.c
@@ -69,8 +69,10 @@ struct timetable
 
 struct client_server
 {// SEPEHR - could you please provide comments for what this struct is/does
+    // struct stores the inputs given into the program for the server
     int browser_port;
-    char name[MAX_WORDSIZE]; //SEPEHR- I've replaced with a pre-defined CONSTANT as best practise - delete this comment once read
+    int query_port;
+    char name[MAX_WORDSIZE]; 
 };
 
 
@@ -156,17 +158,36 @@ void find_route()
 
 // SEPEHR - could you please provide comments?
 void server_listen(struct client_server *my_server){
+
+    // creates socket and binds it to the browser port for TCP connection
     char client_req[1000];
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in _addr;
-    _addr.sin_family = AF_INET;
-    _addr.sin_addr.s_addr = INADDR_ANY;
-    _addr.sin_port = htons(my_server->browser_port);
-    int bind_sock = bind(sock,(struct sockaddr*) &_addr, sizeof(struct sockaddr_in));
+    struct sockaddr_in tcp_addr;
+    //sets it to ipv4
+    tcp_addr.sin_family = AF_INET;
+    //allows it to connect to a nonspecific ip
+    tcp_addr.sin_addr.s_addr = INADDR_ANY;
+    //adds port int to the sockadd_in using hton to convert to correct format
+    tcp_addr.sin_port = htons(my_server->browser_port);
+    int bind_sock = bind(sock,(struct sockaddr*) &tcp_addr, sizeof(struct sockaddr_in));
     int listen_sock = listen(sock, 1);
-    socklen_t addrlen = sizeof(_addr);
+    socklen_t addrlen = sizeof(tcp_addr);
+
+
+    // creates socket and binds it with the query port for UDP connection
+    int sock2 = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in udp_addr;
+    udp_addr.sin_family = AF_INET;
+    udp_addr.sin_addr.s_addr = INADDR_ANY;
+    udp_addr.sin_port = htons(my_server->query_port);
+    int bind_udpsock = bind(sock2,(struct sockaddr*) &udp_addr, sizeof(struct sockaddr_in));
+    int listen_udpsock = listen(sock2, 1);
+    socklen_t addrlen2 = sizeof(udp_addr);
+
+
+
     while(1){
-        int accept_sock = accept(sock, (struct sockaddr*) &_addr, &addrlen);
+        int accept_sock = accept(sock, (struct sockaddr*) &tcp_addr, &addrlen);
         read(accept_sock, client_req, sizeof(client_req));
         // takes out the destination name out of the client_req string
         char* delim= "=";
@@ -195,6 +216,7 @@ void communicate_with_other_server(){
 
 int main(int argc, char const *argv[]){
     struct client_server my_server;
+    my_server.query_port = atoi(argv[3]);
     my_server.browser_port = atoi(argv[2]);
     strcpy(my_server.name, argv[1]);
     server_listen(&my_server);
